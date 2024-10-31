f09ae20328948b7197dd1bde7f24e9e546c1b158
unknown <Agustinkarczz2000@gmail.com>
2024-10-31 16:13:02 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+class Departamento {
+  nombre: string;
+  constructor(nombre: string) {
+    this.nombre = nombre;
+  }
+  getName() {
+    return this.nombre;
+  }
+}
+class Piso {
+  nombre: string;
+  departamentos: Departamento[] = [];
+  constructor(nombre: string) {
+    this.nombre = nombre;
+  }
+  pushDepartamento(departamento: Departamento) {
+    this.departamentos.push(departamento);
+  }
+  getDepartamentos(): Departamento[] {
+    return this.departamentos;
+  }
+}
+class Edificio {
+  pisos: Piso[];
+  constructor(pisos: Piso[]) {
+    this.pisos = pisos;
+  }
+  addDepartamentoToPiso(nombreDePiso: string, departamento: Departamento) {
+    const pisoEncontrado = this.pisos.find((p) => p.nombre == nombreDePiso);
+    return pisoEncontrado.pushDepartamento(departamento);
+  }
+  getDepartamentoByPiso(nombreDePiso: string): Departamento[] {
+    const pisoEncontrado = this.pisos.find((p) => p.nombre == nombreDePiso);
+    return pisoEncontrado.getDepartamentos();
+  }
+}
